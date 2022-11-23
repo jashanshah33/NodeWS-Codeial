c833bb81d5c9ef4b02865125dcda65bb7921c2c4
jashanshah33 <jashanshah33@gmail.com>
2022-11-23 04:10:55 -0500

    controller and router for user
    Signed-off-by: jashanshah33 <jashanshah33@gmail.com>

---
+++ controllers/posts_controller.js
@@ -0,0 +1,3 @@
+module.exports.posts = function (res, res) {
+  return res.end("<h1>Posts</h1>");
+};
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const homeController = require("../controllers/home_controller");
+const postsController = require("../controllers/posts_controller");
 
 console.log("Router LOaded");
 
 router.get("/", homeController.home);
-router.get("/about", homeController.about);
+router.get("/posts", postsController.posts);
+router.use("/users", require("./users"));
 
 module.exports = router;
