f0af6976fc191c86a764652f47bcf3d902665ada
jashanshah33 <jashanshah33@gmail.com>
2022-11-27 02:06:59 -0500

    Partials and Layouts added
    Signed-off-by: jashanshah33 <jashanshah33@gmail.com>

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts')
+
+app.use(expressLayouts)
 
 app.use("/", require("./routes"));
 app.set("view engine", "ejs");
 app.set("views", "./views");
+app.use(express.static("assets"));
 
 app.listen(port, function (err) {
   if (err) {
