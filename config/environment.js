87c1c75f52acc30bf0cb0b7cb68b259ff55961d5
jashanshah33 <jashanshah33@gmail.com>
2022-12-27 02:46:58 -0500

    logger added for production
    Signed-off-by: jashanshah33 <jashanshah33@gmail.com>

--- config/environment.js
+++ config/environment.js
@@ -1,4 +1,16 @@
 require("dotenv").config();
+const fs = require("fs");
+const rfs = require("rotating-file-stream");
+const path = require("path");
+
+const logDirectory = path.join(__dirname, "../production_logs");
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+
+const accessLogStream = rfs.createStream("access.log", {
+  interval: "1d",
+  path: logDirectory,
+}  );
+
 
 const development = {
   name: "development",
@@ -20,27 +32,35 @@ const development = {
   google_client_secret: "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",
   google_callback_URL: "http://localhost:8000/users/auth/google/callback",
   jwt_secret: "JashanSHah",
+  morgan: {
+    mode: "dev",
+    options: { stream: accessLogStream },
+  },
 };
 
 const production = {
   name: "production",
-  asset_path: process.env.asset_path,
-  session_cookie_key: process.env.session_cookie_key,
-  db: process.env.db,
+  asset_path: process.env.SOCIALBOOK_asset_path,
+  session_cookie_key: process.env.SOCIALBOOK_session_cookie_key,
+  db: process.env.SOCIALBOOK_db,
   smtp: {
     service: "gmail",
     host: "smtp.gmail.com",
     port: 587,
     secure: false,
     auth: {
-      user: process.env.smtpUser,
-      pass: process.env.smtpPass,
+      user: process.env.SOCIALBOOK_smtpUser,
+      pass: process.env.SOCIALBOOK_smtpPass,
     },
   },
-  google_client_id: process.env.google_client_id,
-  google_client_secret: process.env.google_client_secret,
-  google_callback_URL: process.env.google_callback_URL,
-  jwt_secret: process.env.jwt_secret,
+  google_client_id: process.env.SOCIALBOOK_google_client_id,
+  google_client_secret: process.env.SOCIALBOOK_google_client_secret,
+  google_callback_URL: process.env.SOCIALBOOK_google_callback_URL,
+  jwt_secret: process.env.SOCIALBOOK_jwt_secret,
+  morgan: {
+    mode: "combined",
+    options: { stream: accessLogStream },
+  },
 };
 
 module.exports =
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const env = require('./config/environment')
+const env = require("./config/environment");
+const logger = require("morgan");
 const cookieParser = require("cookie-parser");
 const app = express();
 const port = 8000;
@@ -8,26 +9,26 @@ const db = require("./config/mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocal = require("./config/passport-local-strategy");
-const passportJWT = require('./config/passport-jwt-strategy')
-const passportGoogle = require('./config/passport-google-Oauth-strategy')
+const passportJWT = require("./config/passport-jwt-strategy");
+const passportGoogle = require("./config/passport-google-Oauth-strategy");
 // const MongoStore = require("connect-mongo")(session); did't work
 const MongoStore = require("connect-mongo");
 const sassMiddleware = require("node-sass-middleware");
 const flash = require("connect-flash");
 const custonMware = require("./config/middleware");
 
-const chatServer = require('http').Server(app);
-const chatSockets = require('./config/chat-sockets').chatSockets(chatServer);
-chatServer.listen(5000)
-console.log('chat server is listening on Port 5000');
+const chatServer = require("http").Server(app);
+const chatSockets = require("./config/chat-sockets").chatSockets(chatServer);
+chatServer.listen(5000);
+console.log("chat server is listening on Port 5000");
 
-const path = require('path')
+const path = require("path");
 
-if (env.name == 'development') {
+if (env.name == "development") {
   app.use(
     sassMiddleware({
-      src: path.join(__dirname,'/' , env.asset_path, '/scss') ,
-      dest: path.join(__dirname,'/' , env.asset_path, '/css'),
+      src: path.join(__dirname, "/", env.asset_path, "/scss"),
+      dest: path.join(__dirname, "/", env.asset_path, "/css"),
       debug: true,
       outputStyle: "expanded",
       prefix: "/css",
@@ -39,6 +40,9 @@ app.use(express.urlencoded());
 app.use(cookieParser());
 app.use(express.static(env.asset_path));
 app.use("/uploads", express.static(__dirname + "/uploads"));
+
+app.use(logger(env.morgan.mode, env.morgan.options));
+
 app.use(expressLayouts);
 app.set("layout extractStyles", true);
 app.set("layout extractScripts", true);
