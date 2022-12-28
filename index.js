dd479610417e08b1635de8271b76edf68e7c1ab8
jashanshah33 <jashanshah33@gmail.com>
2022-12-28 05:45:41 -0500

    Early stage gulf
    Signed-off-by: jashanshah33 <jashanshah33@gmail.com>

--- index.js
+++ index.js
@@ -27,8 +27,8 @@ const path = require("path");
 if (env.name == "development") {
   app.use(
     sassMiddleware({
-      src: path.join(__dirname, "/", env.asset_path, "/scss"),
-      dest: path.join(__dirname, "/", env.asset_path, "/css"),
+      src: path.join(__dirname, env.asset_path, "/scss"),
+      dest: path.join(__dirname, env.asset_path, "/css"),
       debug: true,
       outputStyle: "expanded",
       prefix: "/css",
