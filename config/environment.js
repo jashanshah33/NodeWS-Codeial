2d83a6d4051da5b4f7e229968d97b0e031934fc2
Jashanpreet Singh <104054121+jashanshah33@users.noreply.github.com>
2023-01-28 05:40:58 -0500

    Update environment.js

--- config/environment.js
+++ config/environment.js
@@ -15,23 +15,23 @@ const accessLogStream = rfs.createStream("access.log", {
 const development = {
   name: "development",
   asset_path: "./assets",
-  session_cookie_key: "jashanShah",
-  db: "socialBook_development",
+  session_cookie_key: "",
+  db: "",
   smtp: {
     service: "gmail",
     host: "smtp.gmail.com",
     port: 587,
     secure: false,
     auth: {
-      user: "webdeveloperpb18@gmail.com",
-      pass: "olcwnklsgytumcln",
+      user: "",
+      pass: "",
     },
   },
   google_client_id:
-    "209353265520-nl3qb24gfh5rtb8r03ve2c2ueofut8pb.apps.googleusercontent.com",
-  google_client_secret: "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",
-  google_callback_URL: "http://localhost:8000/users/auth/google/callback",
-  jwt_secret: "JashanSHah",
+    "",
+  google_client_secret: "",
+  google_callback_URL: "",
+  jwt_secret: "",
   morgan: {
     mode: "dev",
     options: { stream: accessLogStream },
